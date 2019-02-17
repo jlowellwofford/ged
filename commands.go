60b6289bd3e02983629c05c594a4aaa98f1f2c61
J. Lowell Wofford <lowell@lanl.gov>
2019-02-17 14:38:58 -0700

    code cleanup; refactor into  multiple files

---
+++ commands.go
@@ -0,0 +1,199 @@
+// commands.go - defines editor commands
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+// A Context is passed to an invoked command
+type Context struct {
+	cmd       string // full command string
+	cmdOffset int    // start of the command after address resolution
+	addrs     []int  // resolved addresses
+}
+
+// A Command can be run with a Context and returns an error
+type Command func(*Context) error
+
+// The cmds map maps single byte commands to their handler functions.
+// This is also a good way to check what commands are implemented.
+var cmds = map[byte]Command{
+	'q': cmdQuit,
+	'Q': cmdQuit,
+	'd': cmdDelete,
+	'p': cmdPrint,
+	'n': cmdPrint,
+	'h': cmdErr,
+	'H': cmdErr,
+	'a': cmdInput,
+	'i': cmdInput,
+	'c': cmdInput,
+	'w': cmdWrite,
+	'W': cmdWrite,
+	'k': cmdMark,
+}
+
+//////////////////////
+// Command handlers /
+////////////////////
+
+func cmdDelete(ctx *Context) (e error) {
+	var r [2]int
+	if r, e = buffer.AddrRangeOrLine(ctx.addrs); e != nil {
+		return
+	}
+	e = buffer.Delete(r)
+	return
+}
+
+func cmdQuit(ctx *Context) (e error) {
+	if ctx.cmd[ctx.cmdOffset] == 'q' && buffer.Dirty() {
+		return fmt.Errorf("warning: file modified")
+	}
+	os.Exit(0)
+	return
+}
+
+func cmdPrint(ctx *Context) (e error) {
+	var r [2]int
+	if r, e = buffer.AddrRangeOrLine(ctx.addrs); e != nil {
+		return
+	}
+	for l := r[0]; l <= r[1]; l++ {
+		if ctx.cmd[ctx.cmdOffset] == 'n' {
+			fmt.Printf("%d\t", l+1)
+		}
+		fmt.Printf("%s\n", buffer.GetMust(l, true))
+	}
+	return
+}
+
+func cmdErr(ctx *Context) (e error) {
+	if ctx.cmd[ctx.cmdOffset] == 'h' {
+		if state.lastErr != nil {
+			fmt.Println(state.lastErr)
+			return
+		}
+	}
+	if ctx.cmd[ctx.cmdOffset] == 'H' {
+		if state.printErr {
+			state.printErr = false
+			return
+		}
+		state.printErr = true
+	}
+	return
+}
+
+func cmdInput(ctx *Context) (e error) {
+	scan := bufio.NewScanner(os.Stdin)
+	nbuf := []string{}
+	if len(ctx.cmd[ctx.cmdOffset+1:]) != 0 && ctx.cmd[ctx.cmdOffset] != 'c' {
+		return fmt.Errorf("%c only takes a single line addres", ctx.cmd[ctx.cmdOffset])
+	}
+	for scan.Scan() {
+		line := scan.Text()
+		if line == "." {
+			break
+		}
+		nbuf = append(nbuf, line)
+	}
+	if len(nbuf) == 0 {
+		return
+	}
+	switch ctx.cmd[ctx.cmdOffset] {
+	case 'i':
+		var line int
+		if line, e = buffer.AddrValue(ctx.addrs); e != nil {
+			return
+		}
+		e = buffer.Insert(line, nbuf)
+	case 'a':
+		var line int
+		if line, e = buffer.AddrValue(ctx.addrs); e != nil {
+			return
+		}
+		e = buffer.Insert(line+1, nbuf)
+	case 'c':
+		var r [2]int
+		if r, e = buffer.AddrRange(ctx.addrs); e != nil {
+			return
+		}
+		buffer.Delete(r)
+		e = buffer.Insert(r[0], nbuf)
+	}
+	return
+}
+
+var rxWrite = regexp.MustCompile("^(q)?(?: )?(!)?(.*)")
+
+func cmdWrite(ctx *Context) (e error) {
+	file := state.fileName
+	quit := false
+	run := false
+	var r [2]int
+	if ctx.cmdOffset == 0 {
+		r[0] = 0
+		r[1] = buffer.Len()
+	} else {
+		if r, e = buffer.AddrRange(ctx.addrs); e != nil {
+			return
+		}
+	}
+	m := rxWrite.FindAllStringSubmatch(ctx.cmd[ctx.cmdOffset+1:], -1)
+	if m[0][1] == "q" {
+		quit = true
+	}
+	if m[0][2] == "!" {
+		run = true
+	}
+	if len(m[0][3]) > 0 {
+		file = m[0][3]
+	}
+	var lstr []string
+	lstr, e = buffer.Get(r)
+	if e != nil {
+		return
+	}
+	if run {
+		return fmt.Errorf("sending to stdin not yet supported")
+	}
+	var f *os.File
+	oFlag := os.O_TRUNC
+
+	if ctx.cmd[ctx.cmdOffset] == 'W' {
+		oFlag = os.O_APPEND
+	}
+	if f, e = os.OpenFile(file, os.O_WRONLY|os.O_CREATE|oFlag, 0666); e != nil {
+		return e
+	}
+	defer f.Close()
+	for _, s := range lstr {
+		_, e = fmt.Fprintf(f, "%s\n", s)
+		if e != nil {
+			return
+		}
+	}
+	if quit {
+		cmdQuit(ctx)
+	}
+	buffer.Clean()
+	return
+}
+
+func cmdMark(ctx *Context) (e error) {
+	if len(ctx.cmd)-1 <= ctx.cmdOffset {
+		e = fmt.Errorf("no mark character supplied")
+		return
+	}
+	c := ctx.cmd[ctx.cmdOffset+1]
+	var l int
+	if l, e = buffer.AddrValue(ctx.addrs); e != nil {
+		return
+	}
+	e = buffer.SetMark(c, l)
+	return
+}
---
+++ filebuffer.go
@@ -0,0 +1,187 @@
+// filebuffer.go - defines the FileBuffer object
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// A FileBuffer manages a file being edited.
+// A FileBuffer never deletes/modifies anything directly until it is replaced.
+// It keeps a map of known lines to the current buffer.
+// Note: FileBuffer is 0-addressed lines, so off-by-one from what `ed` expects.
+type FileBuffer struct {
+	buffer []string // all lines we know about, they never get delited
+	file   []int    // sequence of buffer lines
+	dirty  bool     // tracks if the file has been modifed
+	addr   int      // current file address
+	marks  map[byte]int
+}
+
+// NewFileBuffer creats a new FileBuffer object
+func NewFileBuffer(in []string) *FileBuffer {
+	f := &FileBuffer{
+		buffer: in,
+		file:   []int{},
+		dirty:  false,
+		addr:   0,
+		marks:  make(map[byte]int),
+	}
+	for i := range f.buffer {
+		f.file = append(f.file, i)
+	}
+	return f
+}
+
+// ErrOOB line is out of bounds
+var ErrOOB = fmt.Errorf("line is out of bounds")
+
+// ErrINV address is invalid
+var ErrINV = fmt.Errorf("invalid address")
+
+// OOB checks if a line is out of bounds
+func (f *FileBuffer) OOB(l int) bool {
+	if l < 0 || l >= len(f.file) {
+		return true
+	}
+	return false
+}
+
+// GetMust gets a specified line, to be used when we know it's safe (no error return)
+// if "set" is true, sets the current line pointer
+func (f *FileBuffer) GetMust(line int, set bool) string {
+	if set {
+		f.addr = line
+	}
+	return f.buffer[f.file[line]]
+}
+
+// Get a specified line range
+// this updates the current line pointer
+func (f *FileBuffer) Get(r [2]int) (lines []string, e error) {
+	if f.OOB(r[0]) || f.OOB(r[1]) {
+		e = ErrOOB
+		return
+	}
+	for l := r[0]; l <= r[1]; l++ {
+		lines = append(lines, f.buffer[f.file[l]])
+		f.addr = l
+	}
+	return
+}
+
+// Delete unmaps lines from the file
+func (f *FileBuffer) Delete(r [2]int) (e error) {
+	blines := []int{}
+	for l := r[0]; l <= r[1]; l++ {
+		if f.OOB(l) {
+			return ErrOOB
+		}
+		blines = append(blines, f.file[l])
+	}
+	for _, b := range blines {
+		for i, l := range f.file {
+			if l == b {
+				f.file = append(f.file[:i], f.file[i+1:]...)
+				break
+			}
+		}
+	}
+	f.dirty = true
+	f.addr = r[0] + 1
+	if f.OOB(f.addr) {
+		f.addr = 0
+	}
+	return
+}
+
+// Insert adds nlines to buffer and inserts them at line
+func (f *FileBuffer) Insert(line int, nlines []string) (e error) {
+	if f.OOB(line) {
+		return ErrOOB
+	}
+	if len(nlines) == 0 {
+		return
+	}
+	first := len(f.buffer)
+	f.buffer = append(f.buffer, nlines...)
+	nf := []int{}
+	for i := first; i < len(f.buffer); i++ {
+		nf = append(nf, i)
+	}
+	f.file = append(f.file[:line], append(nf, f.file[line:]...)...)
+	f.dirty = true
+	f.addr = line + len(nlines) - 1
+	return
+}
+
+// Len returns the current file length
+func (f *FileBuffer) Len() int {
+	return len(f.file)
+}
+
+// Dirty returns whether the file has changed
+func (f *FileBuffer) Dirty() bool {
+	return f.dirty
+}
+
+// GetAddr gets the current file addr
+func (f *FileBuffer) GetAddr() int {
+	return f.addr
+}
+
+// SetAddr sets the current addr, errors if OOB
+func (f *FileBuffer) SetAddr(i int) (e error) {
+	if f.OOB(i) {
+		return ErrOOB
+	}
+	f.addr = i
+	return
+}
+
+// Clean resets the dirty flag
+func (f *FileBuffer) Clean() {
+	f.dirty = false
+}
+
+// FileToBuffer reads a file and creates a new FileBuffer from it
+func FileToBuffer(file string) (fb *FileBuffer, e error) {
+	b := []string{}
+	f, e := os.Open(file)
+	if e != nil {
+		e = fmt.Errorf("could not read file: %v", e)
+		return nil, nil
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		b = append(b, s.Text())
+	}
+	fb = NewFileBuffer(b)
+	return
+}
+
+func (f *FileBuffer) SetMark(c byte, l int) (e error) {
+	if f.OOB(l) {
+		e = ErrOOB
+		return
+	}
+	f.marks[c] = f.file[l]
+	return
+}
+
+func (f *FileBuffer) GetMark(c byte) (l int, e error) {
+	bl, ok := f.marks[c]
+	if !ok {
+		return -1, fmt.Errorf("no such mark: %c", c)
+	}
+	for i := 0; i < f.Len(); i++ {
+		if f.file[i] == bl {
+			l = i
+			return
+		}
+	}
+	return -1, fmt.Errorf("mark was cleared: %c", c)
+}
